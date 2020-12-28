89ffa73922d2abd3664a1b3c2ae2ab06f683db96
MinkyuKim <godekd3133@naver.com>
2020-12-28 20:29:43 +0900

    배틀 씬 추가

---
+++ Assets/Scripts/Scene/BattleScene/BattleSceneUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSceneUI : SceneUI
+{
+    public override void OnSceneLoad()
+    {
+        base.OnSceneLoad();
+    }
+
+    public override void OnSceneUnload()
+    {
+        base.OnSceneUnload();
+    }
+
+    public override void OnSceneUpdate()
+    {
+        base.OnSceneUpdate();
+    }
+
+    public override void OnSceneLateUpdate()
+    {
+        base.OnSceneLateUpdate();
+    }
+
+    public override void OnSceneFixedUpdate()
+    {
+        base.OnSceneFixedUpdate();
+    }
+}
